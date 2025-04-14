c85c3e8f24771d5ac67543b44018276fdc681bde
iim-amit <6661kumaramit@gmail.com>
2025-04-14 22:29:36 +0530

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,50 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
+import messageRoutes from "./routes/message.route.js";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { app , server} from "./lib/socket.js";
+import path from "path";
+
+
+
+app.use(express.json({ limit: "50mb" }));  // Adjust limit as needed
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+app.use(cookieParser());
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://localhost:5001"
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}));
+
+
+dotenv.config();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+    app.get("*", (req, res) => { 
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+     });
+     
+}
+
+server.listen(PORT, () =>{
+    console.log("server is running on PORT:" +PORT);
+    connectDB();
+});
---
+++ backend/src/seeds/user.seed.js
@@ -0,0 +1,61 @@
+import { config } from "dotenv";
+import { connectedDB } from "../lib/db.js";
+import User from "../models/user.model.js";
+
+config();
+
+const seedUsers = [
+  // Female Users
+  {
+    email: "olivia.miller@example.com",
+    fullName: "Olivia Miller",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+  {
+    email: "sophia.davis@example.com",
+    fullName: "Sophia Davis",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/3.jpg",
+  },
+  {
+    email: "amelia.garcia@example.com",
+    fullName: "Amelia Garcia",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/8.jpg",
+  },
+
+  // Male Users
+  {
+    email: "james.anderson@example.com",
+    fullName: "James Anderson",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+  {
+    email: "lucas.moore@example.com",
+    fullName: "Lucas Moore",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/4.jpg",
+  },
+  {
+    email: "daniel.rodriguez@example.com",
+    fullName: "Daniel Rodriguez",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/7.jpg",
+  },
+];
+
+const seedDatabase = async () => {
+  try {
+    await connectedDB();
+
+    await User.insertMany(seedUsers);
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+};
+
+// Call the function
+seedDatabase();
